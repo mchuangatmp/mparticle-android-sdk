220117d02820b7d2dfefcfb1fc9a2badb6673aaa
Michael Chuang <mchuang@mparticle.com>
2021-06-15 17:21:32 -0700

    fix: change to single branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["development, ci/semantic_release"],
+  branches: ["ci/semantic_release"],
   tagFormat: "v${version}",
   repositoryUrl: "https://github.com/mchuangatmp/mparticle-android-sdk-internal",
   plugins: [
