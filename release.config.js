3816d86d244f46514acbebfb90f168b8a8a8af64
Michael Chuang <mchuang@mparticle.com>
2021-06-21 12:35:12 -0700

    fix: test CI job in forked repository

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["ci/semantic_release","development", "master"],
+  branches: ["master"],
   tagFormat: "v${version}",
   repositoryUrl: "https://github.com/mchuangatmp/mparticle-android-sdk",
   plugins: [
